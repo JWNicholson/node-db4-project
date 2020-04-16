12a17264f10c26a32cbec4b5df0146221b597649
JW Nicholson <john.nicholson812@gmail.com>
2020-04-16 15:30:19 -0400

    server.js and knexfile.js setup

--- knexfile.js
+++ knexfile.js
@@ -5,40 +5,20 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './data/recipes.db3'
     },
+    useNullAsDefault:true,
+    migrations:{
+      directory:'./data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    //need to ensure foreign key restrictions work
     pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);//turns on the foreign key enforcement
+      }
     }
   }
-
 };
